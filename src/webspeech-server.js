d042a202a51e82588bc761f01d909d85341b02e2
gracekim8782 <gracekim8782@gmail.com>
2022-05-20 14:47:43 +0900

    added Google NLP API

--- src/webspeech-server.js
+++ src/webspeech-server.js
@@ -11,11 +11,8 @@ const port = 3000;
 
 // .env에서 API_KEY 가져오기
 dotenv.config({ path: ".env", encoding: "utf8" });
-const API_KEY = process.env.OPEN_API_KEY;
-
-const openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU";
-// 분석 코드는 개체명 인식으로 설정
-const analysisCode = "ner";
+const ETRI_API_KEY = process.env.ETRI_API_KEY;
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 // 서버 구축 함수
 function setupServer() {
@@ -33,46 +30,75 @@ function setupServer() {
 
   io.on("connect", (socket) => {
     // 클라이언트에서 음성 인식 결과 보낼 때 개체명 인식 함수 호출
-    socket.on("ner", (audioTranscript) => {
-      getNamedEntity(audioTranscript, socket);
+    socket.on("ner", (text) => {
+      etriEntity(text, socket);
+      googleEntity(text, socket);
     });
   });
 }
 
-// ETRI 개체명 인식 API에 접근하여 개체명 추출하기
-async function getNamedEntity(audioTranscript, socket) {
-  var entityList = [];
+// Google Cloud NLP API중 analyzeEntities 사용
+async function googleEntity(text, socket) {
+  googleApiURL = `https://language.googleapis.com/v1/documents:analyzeEntities?key=${GOOGLE_API_KEY}`;
+
+  let postData = {
+    encodingType: "UTF8",
+    document: {
+      type: "PLAIN_TEXT",
+      content: text,
+    },
+  };
+
   var returnText;
-  await axios
-    .post(
-      openApiURL,
-      {
-        access_key: API_KEY,
-        argument: { text: audioTranscript, analysis_code: analysisCode },
-      },
-      { headers: { "Content-Type": "application/json; charset=UTF-8" } }
-    )
-    .then(function (response) {
-      if (typeof response !== "undefined") {
-        const returnObject = response.data;
-        if (returnObject.result == 0) {
-          // 개체명 인식 성공
-          const words = response.data.return_object.sentence[0].NE;
-          // 개체명으로 이루어진 배열 생성
-          for (var word of words) {
-            entityList.push(word.text);
-          }
-          returnText = "개체명 인식 결과: [" + entityList.toString() + "] ";
-        } else {
-          // API 호출은 성공했으나 어떠한 이유로 개체명 인식에 실패 예) API 키가 잘못되었음, 분석할 문장이 없음
-          returnText = "개체명 인식 실패 이유: " + returnObject.reason + " ";
-        }
-      }
-    })
-    .catch(function (error) {
-      // API 호출 실패
-      returnText = "에러: " + error.response.data.reason + " ";
-    });
+
+  try {
+    const response = await axios.post(googleApiURL, postData);
+    // 개체명으로 이루어진 배열 생성
+    var entityList = [];
+    for (var entity of response.data.entities) {
+      entityList.push(entity.name);
+    }
+    returnText = "개체명 인식 결과: [" + entityList.toString() + "] ";
+  } catch (error) {
+    // API 호출 실패
+    returnText = "에러: " + error.response.statusText + " ";
+  }
+
+  // 브라우저로 개체명 인식 결과를 소켓을 통해 반환
+  socket.emit("ner-results", returnText);
+}
+
+// ETRI 개체명 인식 API에 접근하여 개체명 추출하기
+async function etriEntity(text, socket) {
+  const etriApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU";
+  // 분석 코드는 개체명 인식으로 설정
+  const analysisCode = "ner";
+
+  let postData = {
+    access_key: ETRI_API_KEY,
+    argument: { text: text, analysis_code: analysisCode },
+  };
+
+  let config = {
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  };
+
+  var returnText;
+
+  try {
+    // 개체명 인식 ETRI API 호출 성공
+    const response = await axios.post(etriApiURL, postData, config);
+    // 개체명으로 이루어진 배열 생성
+    var entityList = [];
+    for (var entity of response.data.entities) {
+      entityList.push(entity.name);
+    }
+    returnText = "개체명 인식 결과: [" + entityList.toString() + "] ";
+  } catch (error) {
+    // API 호출 실패
+    returnText = "에러: " + error.response.data.reason + " ";
+  }
+
   // 브라우저로 개체명 인식 결과를 소켓을 통해 반환
   socket.emit("ner-results", returnText);
 }
